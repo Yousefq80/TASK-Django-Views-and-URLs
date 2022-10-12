90f87c5c74f4363d7c1afc8e75c9a275aaf4d428
Yousefq80 <ysalharby@gmail.com>
2022-10-12 18:50:16 +0300

    done

--- products/views.py
+++ products/views.py
@@ -1,6 +1,12 @@
+from itertools import product
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def get_home(request):
     return HttpResponse(f"<h1>Welcome</h1>")
+
+def get_product(request,product_id):
+    p= Product.objects.get(id=product_id)
+    return HttpResponse(p)
